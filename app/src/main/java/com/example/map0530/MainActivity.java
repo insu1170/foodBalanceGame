8b44b2b8bdafd53ad6b1789e51f8fd3a1c7b21f4
insu1170 <insu1170@naver.com>
2024-05-31 10:58:05 +0900

    naverMAP 적용 - 기본

--- app/src/main/java/com/example/map0530/MainActivity.java
+++ app/src/main/java/com/example/map0530/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.map0530;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map_container);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.map_container, mapFragment);
+            fragmentTransaction.commit();
+        }
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        // 지도가 준비되었을 때 실행되는 코드
     }
 }
